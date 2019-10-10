8f82fcd0e1193b76b1314fc8a146da8c2c1d6881
jsterly <jsterly@umich.edu>
2019-10-10 14:04:20 -0400

    Tom added duck class

---
+++ duck.java
@@ -0,0 +1,12 @@
+public class duck{
+
+  public fly(){
+    system.out.println("A duck can fly");
+  }//closes fly method
+  public swim(){
+    system.out.println("A duck can swim");
+  }//closes swim method
+  public eat(){
+    system.out.println("A duck can eat");
+  }//closes eat method
+}//closes duck class
